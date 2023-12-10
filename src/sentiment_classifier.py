6ba4cd5c23e7cf9f3e3f7a5d6b093b7ee6b6c7e8
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-12-10 16:07:54 +0000

    moodquote

--- src/sentiment_classifier.py
+++ src/sentiment_classifier.py
@@ -4,15 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": ""},
-        {"role": "user", "content": ""}
-    ],
-    temperature=0.7,
-    max_tokens=150,
-)
+while True:
+    user_text= input("enter your mood:")
+    if(user_text=="exit" or user_text=="quit"):
+        break
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a sentient bot you take the user's input about their mood and print out a quote relating to that mood"},
+            {"role": "user", "content": user_text}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
 
-response_message = response["choices"][0]["message"]
-print(response_message)
+    response_message = response["choices"][0]["message"]
+    print(response_message)
