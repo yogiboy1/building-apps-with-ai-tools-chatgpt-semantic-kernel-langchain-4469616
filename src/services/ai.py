74b24f7dc3469731eb0dc8e6881c8ff2ab6f0e55
yogiboy1 <88436258+yogiboy1@users.noreply.github.com>
2023-12-10 16:53:11 +0000

    care feedback responder

--- src/services/ai.py
+++ src/services/ai.py
@@ -7,9 +7,9 @@ load_dotenv()
 def generate_review(review):
     openai.api_key = os.getenv('OPENAI_API_KEY')
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a sentiment classification bot, print out if the user is happy or sad. Only print out happy or sad."},
+            {"role": "system", "content": "You are a sentiment classification bot working in the healthcare field, print out if the user is happy or sad based on their review on care received . Print only the word happy if they provide a positive feedback.If they are not then identify the gap in care and print out sad along with the name of the gap."},
             {"role": "user", "content": review}
         ],
         temperature=0.7,
@@ -19,6 +19,6 @@ def generate_review(review):
     response_message = response["choices"][0]["message"]["content"]
     if response_message == "happy":
         # TODO 1
-        return ""
+        return "thank you for staying with us"
     # TODO 2
-    return ""
+    return response_message
